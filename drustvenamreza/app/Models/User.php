179d3c97a931d3cde2e657a92912dcb306b064b0
Anica Nedeljkovic <anicanedeljkovic16@gmail.com>
2023-12-19 18:19:42 +0100

    Definisani modeli i veze izmedju njih

--- drustvenamreza/app/Models/User.php
+++ drustvenamreza/app/Models/User.php
@@ -42,4 +42,24 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'user_follows', 'follower_id', 'followed_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follows', 'followed_id', 'follower_id');
+    }
 }
