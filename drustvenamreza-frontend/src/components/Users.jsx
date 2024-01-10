504a730cd575551ef5df6f4f02537a849d13cd20
Anica Nedeljkovic <anicanedeljkovic16@gmail.com>
2024-01-10 21:07:29 +0100

    Kreirana komponenta Users koja nam cuva pratioce za stranicu Followers i uvozi slike iz foldera Assets

---
+++ drustvenamreza-frontend/src/components/Users.jsx
@@ -0,0 +1,41 @@
+import follower1 from '../assets/follower1.jpg'; 
+import follower2 from '../assets/follower2.jpg';
+import follower3 from '../assets/follower3.jpg';
+import follower4 from '../assets/follower4.jpg';
+import follower5 from '../assets/follower5.jpg';
+import follower6 from '../assets/follower6.jpg';
+import follower7 from '../assets/follower7.jpg';
+
+const users = [
+  {
+    username: 'Maja Stankovic',
+    image: follower1,
+  },
+  {
+    username: 'Ana Danicic',
+    image: follower2,
+  },
+  {
+    username: 'Marija Tosic',
+    image: follower3,
+  },
+  {
+    username: 'Nikolina Maric',
+    image: follower4,
+  },
+  {
+    username: 'Tara Jokic',
+    image: follower5,
+  },
+  {
+    username: 'Mina Petrovic',
+    image: follower6,
+  },
+  {
+    username: 'Mona Zdravkovic',
+    image: follower7,
+  },
+  
+];
+
+export default users;
