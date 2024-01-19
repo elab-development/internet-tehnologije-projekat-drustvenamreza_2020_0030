aacb03d4a10ba7c781a9525e5b82aee5ad93901b
andjelamircetic <anicanedeljkovic16@gmail.com>
2024-01-19 21:08:43 +0100

    Uredjeni NavBar i Profile

---
+++ drustvenamreza-projekat/client/src/navbar/Navbar.js
@@ -0,0 +1,245 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// redux state
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import axios from 'axios';
+
+// style
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+  Thermostat,
+} from '@mui/icons-material';
+
+// components
+import FlexBetween from 'components/FlexBetween';
+
+const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const [temp, setTemp] = useState(null);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector((state) => state.user);
+  const isAdmin = useSelector((state) => state.user.admin);
+  const isModerator = useSelector((state) => state.user.moderator);
+  const city = useSelector((state) => state.user.location);
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9665789e88bc99e6fc654ca1d4802a90`
+      );
+      setTemp(res.data.main.temp);
+    };
+
+    getWeather();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <FlexBetween padding='1rem 6%' backgroundColor={alt}>
+      <FlexBetween gap='1.75rem'>
+        <Typography
+          fontWeight='bold'
+          fontSize='clamp(1rem, 2rem, 2.25rem)'
+          color='primary'
+          onClick={() => navigate('/home')}
+          sx={{
+            '&:hover': {
+              color: primaryLight,
+              cursor: 'pointer',
+            },
+          }}
+        >
+          Sociopedia
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius='9px'
+            gap='3rem'
+            padding='0.1rem 1.5rem'
+          >
+            <InputBase placeholder='Search...' />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+
+      {/* DESKTOP NAV */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap='2rem'>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkMode sx={{ fontSize: '25px' }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: '25px' }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: '25px' }} />
+          <Notifications sx={{ fontSize: '25px' }} />
+          <Help sx={{ fontSize: '25px' }} />
+          {temp && (
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://openweathermap.org/api'
+            >
+              <IconButton>
+                <Thermostat sx={{ fontSize: '25px' }} />
+                <Typography>{temp.toFixed(0)} °C</Typography>
+              </IconButton>
+            </a>
+          )}
+
+          <FormControl variant='standard' value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neutralLight,
+                width: '150px',
+                borderRadius: '0.25rem',
+                p: '0.25rem 1rem',
+                '& .MuiSvgIcon-root': {
+                  pr: '0.25rem',
+                  width: '3rem',
+                },
+                '& .MuiSelect-select:focus': {
+                  backgroundColor: neutralLight,
+                },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              {(isAdmin || isModerator) && (
+                <MenuItem onClick={() => navigate('/admin')}>
+                  Dashboard
+                </MenuItem>
+              )}
+              <MenuItem onClick={() => dispatch(setLogout())}>Logout</MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+        >
+          <Menu />
+        </IconButton>
+      )}
+
+      {/* MOBILE NAV */}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position='fixed'
+          right='0'
+          bottom='0'
+          height='100%'
+          zIndex='10'
+          maxWidth='500px'
+          minWidth='300px'
+          backgroundColor={background}
+        >
+          {/* CLOSE ICON */}
+          <Box display='flex' justifyContent='flex-end' p='1rem'>
+            <IconButton
+              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+              <Close />
+            </IconButton>
+          </Box>
+
+          {/* MENU ITEMS */}
+          <FlexBetween
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            gap='3rem'
+          >
+            <IconButton
+              onClick={() => dispatch(setMode())}
+              sx={{ fontSize: '25px' }}
+            >
+              {theme.palette.mode === 'dark' ? (
+                <DarkMode sx={{ fontSize: '25px' }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: '25px' }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: '25px' }} />
+            <Notifications sx={{ fontSize: '25px' }} />
+            <Help sx={{ fontSize: '25px' }} />
+            <FormControl variant='standard' value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: '150px',
+                  borderRadius: '0.25rem',
+                  p: '0.25rem 1rem',
+                  '& .MuiSvgIcon-root': {
+                    pr: '0.25rem',
+                    width: '3rem',
+                  },
+                  '& .MuiSelect-select:focus': {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                {isAdmin && (
+                  <MenuItem onClick={() => navigate('/admin')}>
+                    Dashboard
+                  </MenuItem>
+                )}
+                <MenuItem onClick={() => dispatch(setLogout())}>
+                  Logout
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        </Box>
+      )}
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
